7a80a5299d2402ff94af6fe21866e2182553a4a8
Tom Matthews <trcm@pm.me>
2025-06-09 16:24:59 +0100

    run early if possible

--- lse-filter.user.js
+++ lse-filter.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         lse.co.uk - Remove Share Chat Filtered Messages
 // @namespace    http://tampermonkey.net/
-// @version      0.2.0
-// @description  Removes several annoying div classes
+// @version      0.2.1
+// @description  Removes every annoying div class
 // @author       Tom Matthews
 // @include      https://www.lse.co.uk/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=lse.co.uk
 // @downloadURL  https://tomtastic.github.io/userscripts/lse-filter.user.js
 // @updateURL    https://tomtastic.github.io/userscripts/lse-filter.user.js
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
@@ -27,7 +28,7 @@
     ];
 
     // Function to remove all div elements with the specified classes
-    const removeFilteredMessages = () => {
+    const removeFilter = () => {
         classesToRemove.forEach(className => {
             const elements = document.querySelectorAll(`div.${className}`);
             elements.forEach((element) => {
@@ -37,16 +38,20 @@
         });
     };
 
-    // Run the removal function when the page is fully loaded
-    window.addEventListener('load', () => {
-        removeFilteredMessages();
-    });
-
-    // Optionally, observe for dynamically added content
+    // Run immediately and also when DOM changes
     const observer = new MutationObserver(() => {
-        removeFilteredMessages();
+        removeFilter();
     });
 
-    observer.observe(document.body, { childList: true, subtree: true });
+    // Start observing when DOM is ready
+    document.addEventListener('DOMContentLoaded', () => {
+        removeFilter();
+        observer.observe(document.body, { childList: true, subtree: true });
+    });
+
+    // Try to run early in case the element is already present
+    if (document.readyState !== 'loading') {
+        removeFilter();
+    }
 
 })();
